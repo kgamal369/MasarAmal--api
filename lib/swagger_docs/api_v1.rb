62322af5bb121eca4604690f260b860be7977700
Karim Gamal Elsayed <karim.elsayed@sms-digital.com>
2023-12-24 03:16:16 +0100

    fix swagger api

--- lib/swagger_docs/api_v1.rb
+++ lib/swagger_docs/api_v1.rb
@@ -5,7 +5,7 @@ class Swagger::Docs::Config
     end
   end
   
-  Swagger::Docs::Config.base_api_controller = ActionController::API
+  Swagger::Docs::Config.base_api_controller = ActionController::Base
   Swagger::Docs::Config.register_apis({
     "1.0" => {
       controller_base_path: "",
